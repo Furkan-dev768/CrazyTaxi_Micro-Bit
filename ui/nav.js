e47a75c26cbfe7bf07d97d341cf03bf895ca5baf
Furkan-dev768 <pro.fgamer768@gmail.com>
2025-12-12 15:21:55 +0100

    Update nav.js

--- ui/nav.js
+++ ui/nav.js
@@ -1,1 +1,13 @@
+const toggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
 
+toggle.addEventListener('click', () => {
+    menu.classList.toggle('open');
+});
+
+document.addEventListener('click', function(event) {
+    if (!event.target.closest('.navigation') && menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        toggle.classList.remove('open');
+    }
+});
